9e56c5bda0a6245d53e448f6bbbf66fa95e6154f
Genisson Emilio <esgenisson@gmail.com>
2025-12-31 15:16:19 -0300

    add random logic

--- scenes/level.gd
+++ scenes/level.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
+# 1. Carregar a cena
+var meteor_scene: PackedScene = load("res://scenes/meteor.tscn");
 
-
-func _on_timer_timeout() -> void:
-		print("meteor");
+func _on_meteor_timer_timeout() -> void:
+	# 2. Criando uma instância
+	var meteor = meteor_scene.instantiate();
+	
+	# 3. Atribuir o node a scene tree
+	$Meteors.add_child(meteor);
--- scenes/meteor.gd
+++ scenes/meteor.gd
@@ -1,10 +1,17 @@
 extends Area2D
 
 
+func _ready() -> void:
+	var rng := RandomNumberGenerator.new();
+	
+	# Start position
+	var width: float = get_viewport().get_visible_rect().size[0];
+	var random_x: float = rng.randi_range(0, width);
+	var random_y: float = rng.randi_range(-150, -50);
+	position = Vector2(random_x, random_y);
+
+func _process(delta: float) -> void:
+	position += Vector2(0, 1.0) * 400 * delta;
 
 func _on_body_entered(body: Node2D) -> void:
 	print("Body entered");
-
-
-func _on_timer_timeout() -> void:
-	print("foi foi")
